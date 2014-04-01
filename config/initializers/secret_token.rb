1df53cfd96dd7cc2e36745ecda3d6d8de0723c18
svirpav <pavel.svirida@jotautomation.com>
2014-04-01 18:49:42 +0200

    Initial commit
    Signed-off-by: svirpav <pavel.svirida@jotautomation.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JotCssReport::Application.config.secret_key_base = 'd2b7d2ad051e0eb95de2fe03f9e45a4fa4a17a66bb0973702a232ccb4e464eba75e01c81775662a5f5fdcf1d287117e3f4f860e03eb0c8c54d53fecc32d5deab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JotCssReport::Application.config.session_store :cookie_store, key: '_jot_css_report_session'
