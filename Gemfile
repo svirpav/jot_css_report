cb7321145ae1431cb3be80afba2e3bd726b11f46
svirpav <pavel.svirida@jotautomation.com>
2014-04-15 01:13:31 +0200

    test for users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.1.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.1.2'
+gem 'minitest'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410124808) do
+ActiveRecord::Schema.define(version: 20140414203110) do
 
   create_table "equipment", force: true do |t|
     t.string   "jcode"
@@ -42,5 +42,7 @@ ActiveRecord::Schema.define(version: 20140410124808) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
