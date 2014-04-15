c3c0ecb0194874aafae7beabc53b2748afa50a93
svirpav <pavel.svirida@jotautomation.com>
2014-04-15 21:32:27 +0200

    Created basic user model with secure password

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "UserPages" do
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com", password: "test", password_confirmation: "test")
+		@user = User.new(name: "Example User", email: "user@example.com", password: "example1", password_confirmation: "example1")
 	end
 
 	subject{ @user}
@@ -12,6 +12,7 @@ describe "UserPages" do
 	it { should respond_to(:password_digest)}
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation)}
+	it { should respond_to(:authenticate)}
 
 	it { should be_valid }
 
@@ -65,4 +66,25 @@ describe "UserPages" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+  before { @user.save }
+  let(:found_user) { User.find_by(email: @user.email) }
+
+  describe "with valid password" do
+    it { should eq found_user.authenticate(@user.password) }
+  end
+
+  describe "with invalid password" do
+    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+    it { should_not eq user_for_invalid_password }
+    specify { expect(user_for_invalid_password).to be_false }
+  end
+end
 end
