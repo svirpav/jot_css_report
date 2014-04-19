e4503a71c9c5008933cf4e805203d3c113117d90
svirpav <pavel.svirida@jotautomation.com>
2014-04-19 16:17:51 +0200

    added precompile asset to production.rb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,3 @@
 //= require bootstrap
 //= require_tree .
 
-config.assets.precompile += %w( application.js )
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ JotCssReport::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.force_ssl = true
+  config.assets.precompile += %w( application.js )
 end
